f816814888c53c647bfd804772c4a29c1a4bf62d
VanshNyati <vansh.nyati@learner.manipal.edu>
2025-01-02 02:40:33 +0530

    config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
             script: "server.js",
             env: {
                 NODE_ENV: "production",
-                PORT: 5000,
-                MONGODB_URI: "mongodb + srv://vanshnyati17:Z79PI5jGdTaxmoVO@baylink-project.i4w3g.mongodb.net/baylink",
+                PORT: 80,
+                MONGODB_URI: "mongodb+srv://vanshnyati17:Z79PI5jGdTaxmoVO@baylink-project.i4w3g.mongodb.net/baylink",
                 AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
                 AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
                 S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
